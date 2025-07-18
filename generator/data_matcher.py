8494161925df9cfb5943b1318c1acad01c32a87b
tajoumaru <tajoumaru@proton.me>
2025-07-18 13:10:02 +0000

    formatting

--- generator/data_matcher.py
+++ generator/data_matcher.py
@@ -7,7 +7,6 @@ import json
 import os
 from typing import List, Dict, Optional, Tuple
 from multiprocessing import Pool, cpu_count
-from dataclasses import asdict
 
 from fuzzywuzzy import fuzz
 from slugify import slugify
